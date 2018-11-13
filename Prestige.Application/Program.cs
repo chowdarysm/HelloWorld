c7930145c6235a51b6158135c0081bb6b4a0cdf6
sunilmaddipatla <sunil@appguard.us>
2018-11-12 22:15:03 -0500

    Add project files.

---
+++ Prestige.Application/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Prestige.Core.Authorization;
+
+namespace Prestige.Application
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {           
+
+            if (Environment.UserInteractive)
+            {
+                // running as console app
+                Start(args);
+               
+                Stop();
+            }
+            else
+            {
+                // running as service
+                //using (var service = new Service())
+                //{
+                //    ServiceBase.Run(service);
+                //}
+            }
+        }
+
+        public static void Start(string[] args)
+        {
+            
+            try
+            {
+                //GetInfo();
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                //Handle exception or write "throw new InvalidOperationException"..
+            }
+        }
+
+        public static  void GetInfo()
+        {
+            //AuthManager _authManager = new AuthManager();
+            //Console.WriteLine(_authManager.GetInfo());
+        }
+
+        public static void Stop()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
